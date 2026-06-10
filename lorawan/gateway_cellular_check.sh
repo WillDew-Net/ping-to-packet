089f2f13e18cd117de87e4beb402464996d7836d
William Dew <wdew@Mac-2817.lan>
2026-06-09 20:08:27 -0400

    FIX: remove Function keyword due to syntax error

--- lorawan/gateway_cellular_check.sh
+++ lorawan/gateway_cellular_check.sh
@@ -4,7 +4,7 @@ echo ""
 echo "===== Centegix Gateway Connectivity Check ====="
 
 # Function for getting EUI 
-function get_gateway_id(){
+get_gateway_id(){
     # find file location that has gateway_id: /etc/config/lora_pkt_fwd
     # extract gateway_id
     # echo gateway_id 
